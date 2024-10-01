ea4672f579e23ef0bd2e076164ce8a468609081d
revaprama <pramaulidiareva@gmail.com>
2024-10-01 13:07:49 +0700

    modul1_240441100131_revapramaulidia_KakAspraksarahmanuellalumbangaol

---
+++ modul 2/240441100131_reva.py
@@ -0,0 +1,69 @@
+# print("nama=udin")
+# print("umur=18")
+
+# print(f"umur :{"umur"}")
+# nama_baru = "udin" #snakecase
+# namaBaru = "udin" #camelcase
+
+# # IF STATEMENT
+# a = 11
+
+# # # if (kondisi) :
+# if a == 10 :
+#  print("a adalah 10")
+
+# # # elif (kondisi) :
+# if a == 10 :
+#  print("a adalah 10")
+# # elif a == 11 :
+#  print("a adalah 11")
+# elif a == 12 :
+#     print("a adalah 12")
+
+# # #else:
+#     # if a ==10 :
+# #  print("a adalah 110")
+# # else:
+# #  print("a bukan 10")
+
+# angka= int (input("Masukan angka : "))
+# print(angka + 1)
+
+# angka = int (input("masukan angka :"))
+
+# if (angka % 2 == 0) :
+#     print ("angka genap")
+    
+# else:
+#     print("angka ganjil")
+
+# # perulangan (while)
+# a= 1
+
+# while(a<5) :
+#     if a == 3:
+#         break 
+
+#     print(a)
+#     a=a + 1
+# a=1     
+# while(a < 5):
+#     a= a + 1
+#     if a == 3:
+#         pass
+#     print (a)
+
+# # for i in range (start,stop,step):
+# # kondisi 1 parameter
+# for i in range (30):
+#         print(i)
+
+# #  kondisi 2 parameter
+# for i in range (10,20):
+#     print(i)
+# #kondisi 3 parameter
+# for i in range (1,20,2):
+#     print (i)
+
+
+
