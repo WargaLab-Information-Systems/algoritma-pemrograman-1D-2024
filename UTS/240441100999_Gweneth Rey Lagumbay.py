276ecc9893ae23e75a279fa92851c9a19c9f1e83
gwenethrey <gwenethrey@gmail.com>
2024-10-16 19:14:47 +0700

    240441100999_Gweneth Rey Lagumbay.py

--- UTS/240441100999_Gweneth Rey Lagumbay.py
+++ UTS/240441100999_Gweneth Rey Lagumbay.py
@@ -1,11 +1,1 @@
-#diminta untuk mencari apakah sudah makan/mandi atau belum
 
-sudah_mandi = 30
-belum_makan = 15
-
-
-while True :
-    print("kamu terlambat")
-
-
-#maaf kak saya ga ada basic buat ngoding, soalnya waktu SMK saya jurusan keperawatan :(
