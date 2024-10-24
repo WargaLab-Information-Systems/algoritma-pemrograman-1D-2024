e0098a5b8e97ad4c1eebeddd2a8eb12d93aa31ad
Xomniacs <revannovanda2005@gmail.com>
2024-10-24 09:15:26 +0700

    https://github.com/Xomniacs/algoritma-pemrograman-1D-2024.git

---
+++ modul 1/240441100147_RevanNovandaAnanditaYahya/[Pengubahan mata uang].py
@@ -0,0 +1,11 @@
+# # [Pengubahan mata uang]
+# print("DIKETAHUI")
+# print("Uang 1 = 35$")
+# print("Uang 2 = Rp. 15175.40")
+
+# uang1 = 35
+# uang2 = 15175.40
+
+# hasil = uang1 * uang2
+
+# print("Jadi jumlah mata uang pada tanggal 25 adalah", hasil)
