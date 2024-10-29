1279b4e0d45b915df573df2274002317c69a3aed
jihan hanifa <jihanhanifaaa28@gmail.com>
2024-10-29 17:39:22 +0700

    240441100123_JihanIhsaniHanifa_Modul1_Asprak_KakSarah

--- modul 1/240441100083_Salmanalfarisiy_modul1_kakyusri/soal nomer 3.py
+++
@@ -1,6 +0,0 @@
-#soal nomer 3
-dolar = 35
-rupiah = 15.26193
-tukar =float( dolar * rupiah)
-
-print(float(tukar))
--- modul 1/240441100083_Salmanalfarisiy_modul1_kakyusri/soal nomer 4.py
+++
@@ -1,8 +0,0 @@
-#soal nomer 4
-
-n = 7
-r = 4
-import math
-jumlah_cara = math.factorial(n)/(math.factorial(r)*math.factorial(n-r))
-print(f"jumlah cara untuk membuat tim: {jumlah_cara}")
-print(math.factorial(n))
