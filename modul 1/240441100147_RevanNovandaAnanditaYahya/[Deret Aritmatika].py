f81cb5ed968f7258b5f6ad670822be6e2e873fc3
Xomniacs <revannovanda2005@gmail.com>
2024-10-30 23:25:47 +0700

    Modul1

--- modul 1/240441100147_RevanNovandaAnanditaYahya/[Deret Aritmatika].py
+++ modul 1/240441100147_RevanNovandaAnanditaYahya/[Deret Aritmatika].py
@@ -1,26 +1,26 @@
-# # [DERET ARITMATIKA]
-# print("DIKETAHUI")
-# print("Suku ke-5 = 11")
-# print("Jumlah suku ke-8 dan ke-12 = 52")
+# [DERET ARITMATIKA]
+print("DIKETAHUI")
+print("Suku ke-5 = 11")
+print("Jumlah suku ke-8 dan ke-12 = 52")
 
-# print("Membuat persamaan")
-# print("Dari data yang didapat kita bisa membuat persamaan")
-# print("a + 4b = 11")
-# print("2a + 18b = 52")
+print("Membuat persamaan")
+print("Dari data yang didapat kita bisa membuat persamaan")
+print("a + 4b = 11")
+print("2a + 18b = 52")
 
-# print("Menyelesaikan persamaan")
+print("Menyelesaikan persamaan")
 
-# b = (26 - 11) / (9 - 4)
-# a = 11 - 4 * b
+b = (26 - 11) / (9 - 4)
+a = 11 - 4 * b
 
-# print("Nilai a", int(a))
-# print("Nilai b", int(b))
+print("Nilai a", int(a))
+print("Nilai b", int(b))
 
-# print("Mencari jumlah-8 suku pertama")
-# print("Rumus = n/2 * 2 * a + n-1 * b")
+print("Mencari jumlah-8 suku pertamanya")
+print("Rumus = n/2 * 2 * a + n-1 * b")
 
-# n = 8
+n = 8
 
-# hasil = (n/2) * (2*a + (n-1)*b)
+hasil = (n/2) * (2*a + (n-1)*b)
 
-# print("Jumlah 8 suku pertama:", (int(hasil)))
+print("Jumlah 8 suku pertama:", (int(hasil)))
