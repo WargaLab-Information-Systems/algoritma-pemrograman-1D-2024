66a203f2c579acfb64fdc7437f58d8f24218d870
nindasihotang <nindasihotang@gmail.com>
2024-11-11 10:43:20 +0700

    240441100003_NINDAARTIKA_UTS_KKPUTRI

---
+++ UTS/240441100003_NINDAARTIKA_UTS_KKPUTRI/240441100003_NINDA ARTIKA SARI BR SIHOTANG_UTS_ALPRO.py
@@ -0,0 +1,22 @@
+a = "makan"
+b = " mandi"
+c = "motor"
+A_makan = 15 
+B = 0
+C_motor = 15
+input("apakah kamu sudah makan?")
+input("apakah kamu sudah mandi?")
+input("transportasi apa yang kamu pakai?")
+ 
+A_makan = 15
+C_motor = 15
+hasil= (A_makan + C_motor)
+if  A_makan + C_motor   :
+    print("waktu yang kamu butuhkan untuk persiapan", hasil,"menit" " kamu berangkat tepat waktu")
+else :
+    print("kamu terlambat")
+
+
+
+ 
+
